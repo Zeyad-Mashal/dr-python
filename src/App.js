c7b1b4616aa96e4a607724d86b7e1a799569cb55
Mashal <zyadomar112@gmail.com>
2024-08-29 18:41:25 +0300

    basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
 
-      <Router>
+      <Router basename='dr-python'>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/lectures" element={<Home />} />
